8de3925e32da042b47a4f8e08ef36ee880089509
jamie082 <jamiemorrissey02@gmail.com>
2023-03-10 22:02:13 -0800

    added source files

---
+++ file.py
@@ -0,0 +1,19 @@
+# Python code demonstrate to create a
+# Pandas DataFrame with lists of
+# dictionaries as well as
+# row and column indexes.
+  
+import pandas as pd
+  
+# List1
+
+dict = [['chemical', 'executive', 'recruiter', '>100', 'Australia', 'Chemical', '1/15/23'],
+        ['chemical', 'executive', 'recruiter', '>100', 'Canada', 'Chemical', '1/15/23'],
+        ['chemical', 'executive', 'recruiter', '>100', 'Germany', 'Chemical', '1/15/23']]
+
+
+df = pd.DataFrame(dict, columns = ['Keyword', 'Rank', 'URL', 'Location', 'Volume', 'Tags', 'date_scraped'])
+
+
+# print dataframe
+print(df)
